298011788e11933c1c1a22a005e6effba20c28ec
Montassar <Montassar@MONTASSAR-PC>
2021-11-14 20:10:31 +0100

    CultureInfo

--- REM/REM.Konsole/Program.cs
+++ REM/REM.Konsole/Program.cs
@@ -3,16 +3,22 @@ using REM.Model.Entities;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Globalization;
 using System.IO;
 
 namespace REM.Konsole
 {
     class Program
     {
+        // a implémenter
+        private decimal ToDecimal(string rowData)
+        {
+            return 0;
+        }
         static void Main(string[] args)
         {
 
-            var dataTable = GetDataTable(@"C:\Users\karim\source\repos\RenevableEnergyMonitoring\REM\REM.Konsole\CSV\Daily_STEG_09_2011_06_12.csv", ';');
+            var dataTable = GetDataTable(@"C:\Users\Montassar\source\repos\RenevableEnergyMonitoring\REM\REM.Konsole\CSV\Daily_STEG_09_2011_06_12.csv", ';');
 
             using (var db = new REMContext())
             {
@@ -23,11 +29,11 @@ namespace REM.Konsole
                     DateTime timestamp = DateTime.Parse(row["Timestamp"].ToString());
                     var meteo = new Meteo();
                     meteo.Timestamp = timestamp;
-                    meteo.Irradiance = Convert.ToDecimal(row["irradianza"].ToString());
-                    meteo.TemperatureOfModule = Convert.ToDecimal(row["t_modulo"].ToString());
-                    meteo.TemperatureAmbiante = Convert.ToDecimal(row["t_ambiente"].ToString());
-                    meteo.Humidity = Convert.ToDecimal(row["umidita"].ToString());
-                    meteo.SpeedOfWind = Convert.ToDecimal(row["Velocita_vento"].ToString());
+                    meteo.Irradiance = Convert.ToDecimal(row["irradianza"].ToString(), new CultureInfo("en-US"));
+                    meteo.TemperatureOfModule = Convert.ToDecimal(row["t_modulo"].ToString(), new CultureInfo("en-US"));
+                    meteo.TemperatureAmbiante = Convert.ToDecimal(row["t_ambiente"].ToString(), new CultureInfo("en-US"));
+                    meteo.Humidity = Convert.ToDecimal(row["umidita"].ToString(), new CultureInfo("en-US"));
+                    meteo.SpeedOfWind = Convert.ToDecimal(row["Velocita_vento"].ToString(), new CultureInfo("en-US"));
                     meteo.Onduleurs = new List<Onduleur>();
 
                     //Ond1;
@@ -35,12 +41,12 @@ namespace REM.Konsole
                     {
                         Timestamp = timestamp,
                         Designation = "Onduleur1",
-                        VL_Vac = Convert.ToDecimal(row["VL_Vac01"].ToString()),
-                        VL_Fac = Convert.ToDecimal(row["VL_Fac01"].ToString()),
-                        VL_Pac = Convert.ToDecimal(row["VL_Pac01"].ToString()),
-                        VL_Vdc = Convert.ToDecimal(row["VL_Vdc01_01"].ToString()),
-                        VL_Iac = Convert.ToDecimal(row["VL_Iac01"].ToString()),
-                        Total = Convert.ToDecimal(row["VL_ETotal01"].ToString()),
+                        VL_Vac = Convert.ToDecimal(row["VL_Vac01"].ToString(), new CultureInfo("en-US")),
+                        VL_Fac = Convert.ToDecimal(row["VL_Fac01"].ToString(), new CultureInfo("en-US")),
+                        VL_Pac = Convert.ToDecimal(row["VL_Pac01"].ToString(), new CultureInfo("en-US")),
+                        VL_Vdc = Convert.ToDecimal(row["VL_Vdc01_01"].ToString(), new CultureInfo("en-US")),
+                        VL_Iac = Convert.ToDecimal(row["VL_Iac01"].ToString(), new CultureInfo("en-US")),
+                        Total = Convert.ToDecimal(row["VL_ETotal01"].ToString(), new CultureInfo("en-US")),
 
                     };
                     meteo.Onduleurs.Add(onduleur1);
@@ -50,12 +56,12 @@ namespace REM.Konsole
                     {
                         Timestamp = timestamp,
                         Designation = "Onduleur2",
-                        VL_Vac = Convert.ToDecimal(row["VL_Vac02"].ToString()),
-                        VL_Fac = Convert.ToDecimal(row["VL_Fac02"].ToString()),
-                        VL_Pac = Convert.ToDecimal(row["VL_Pac02"].ToString()),
-                        VL_Vdc = Convert.ToDecimal(row["VL_Vdc02_01"].ToString()),
-                        VL_Iac = Convert.ToDecimal(row["VL_Iac02"].ToString()),
-                        Total = Convert.ToDecimal(row["VL_ETotal02"].ToString()),
+                        VL_Vac = Convert.ToDecimal(row["VL_Vac02"].ToString(), new CultureInfo("en-US")),
+                        VL_Fac = Convert.ToDecimal(row["VL_Fac02"].ToString(), new CultureInfo("en-US")),
+                        VL_Pac = Convert.ToDecimal(row["VL_Pac02"].ToString(), new CultureInfo("en-US")),
+                        VL_Vdc = Convert.ToDecimal(row["VL_Vdc02_01"].ToString(), new CultureInfo("en-US")),
+                        VL_Iac = Convert.ToDecimal(row["VL_Iac02"].ToString(), new CultureInfo("en-US")),
+                        Total = Convert.ToDecimal(row["VL_ETotal02"].ToString(), new CultureInfo("en-US")),
                     };
                     meteo.Onduleurs.Add(onduleur2);
 
@@ -64,12 +70,12 @@ namespace REM.Konsole
                     {
                         Timestamp = timestamp,
                         Designation = "Onduleur3",
-                        VL_Vac = Convert.ToDecimal(row["VL_Vac03"].ToString()),
-                        VL_Fac = Convert.ToDecimal(row["VL_Fac03"].ToString()),
-                        VL_Pac = Convert.ToDecimal(row["VL_Pac03"].ToString()),
-                        VL_Vdc = Convert.ToDecimal(row["VL_Vdc03_01"].ToString()),
-                        VL_Iac = Convert.ToDecimal(row["VL_Iac03"].ToString()),
-                        Total = Convert.ToDecimal(row["VL_ETotal03"].ToString()),
+                        VL_Vac = Convert.ToDecimal(row["VL_Vac03"].ToString(), new CultureInfo("en-US")),
+                        VL_Fac = Convert.ToDecimal(row["VL_Fac03"].ToString(), new CultureInfo("en-US")),
+                        VL_Pac = Convert.ToDecimal(row["VL_Pac03"].ToString(), new CultureInfo("en-US")),
+                        VL_Vdc = Convert.ToDecimal(row["VL_Vdc03_01"].ToString(), new CultureInfo("en-US")),
+                        VL_Iac = Convert.ToDecimal(row["VL_Iac03"].ToString(), new CultureInfo("en-US")),
+                        Total = Convert.ToDecimal(row["VL_ETotal03"].ToString(), new CultureInfo("en-US")),
                     };
                     meteo.Onduleurs.Add(onduleur3);
 
@@ -78,12 +84,12 @@ namespace REM.Konsole
                     {
                         Timestamp = timestamp,
                         Designation = "Onduleur4",
-                        VL_Vac = Convert.ToDecimal(row["VL_Vac04"].ToString()),
-                        VL_Fac = Convert.ToDecimal(row["VL_Fac04"].ToString()),
-                        VL_Pac = Convert.ToDecimal(row["VL_Pac04"].ToString()),
-                        VL_Vdc = Convert.ToDecimal(row["VL_Vdc04_01"].ToString()),
-                        VL_Iac = Convert.ToDecimal(row["VL_Iac04"].ToString()),
-                        Total = Convert.ToDecimal(row["VL_ETotal04"].ToString()),
+                        VL_Vac = Convert.ToDecimal(row["VL_Vac04"].ToString(), new CultureInfo("en-US")),
+                        VL_Fac = Convert.ToDecimal(row["VL_Fac04"].ToString(), new CultureInfo("en-US")),
+                        VL_Pac = Convert.ToDecimal(row["VL_Pac04"].ToString(), new CultureInfo("en-US")),
+                        VL_Vdc = Convert.ToDecimal(row["VL_Vdc04_01"].ToString(), new CultureInfo("en-US")),
+                        VL_Iac = Convert.ToDecimal(row["VL_Iac04"].ToString(), new CultureInfo("en-US")),
+                        Total = Convert.ToDecimal(row["VL_ETotal04"].ToString(), new CultureInfo("en-US")),
                     };
                     meteo.Onduleurs.Add(onduleur4);
 
@@ -92,12 +98,12 @@ namespace REM.Konsole
                     {
                         Timestamp = timestamp,
                         Designation = "Onduleur5",
-                        VL_Vac = Convert.ToDecimal(row["VL_Vac05"].ToString()),
-                        VL_Fac = Convert.ToDecimal(row["VL_Fac05"].ToString()),
-                        VL_Pac = Convert.ToDecimal(row["VL_Pac05"].ToString()),
-                        VL_Vdc = Convert.ToDecimal(row["VL_Vdc05_01"].ToString()),
-                        VL_Iac = Convert.ToDecimal(row["VL_Iac05"].ToString()),
-                        Total = Convert.ToDecimal(row["VL_ETotal05"].ToString()),
+                        VL_Vac = Convert.ToDecimal(row["VL_Vac05"].ToString(), new CultureInfo("en-US")),
+                        VL_Fac = Convert.ToDecimal(row["VL_Fac05"].ToString(), new CultureInfo("en-US")),
+                        VL_Pac = Convert.ToDecimal(row["VL_Pac05"].ToString(), new CultureInfo("en-US")),
+                        VL_Vdc = Convert.ToDecimal(row["VL_Vdc05_01"].ToString(), new CultureInfo("en-US")),
+                        VL_Iac = Convert.ToDecimal(row["VL_Iac05"].ToString(), new CultureInfo("en-US")),
+                        Total = Convert.ToDecimal(row["VL_ETotal05"].ToString(), new CultureInfo("en-US")),
                     };
                     meteo.Onduleurs.Add(onduleur5);
 
@@ -106,12 +112,12 @@ namespace REM.Konsole
                     {
                         Timestamp = timestamp,
                         Designation = "Onduleur6",
-                        VL_Vac = Convert.ToDecimal(row["VL_Vac06"].ToString()),
-                        VL_Fac = Convert.ToDecimal(row["VL_Fac06"].ToString()),
-                        VL_Pac = Convert.ToDecimal(row["VL_Pac06"].ToString()),
-                        VL_Vdc = Convert.ToDecimal(row["VL_Vdc06_01"].ToString()),
-                        VL_Iac = Convert.ToDecimal(row["VL_Iac06"].ToString()),
-                        Total = Convert.ToDecimal(row["VL_ETotal06"].ToString()),
+                        VL_Vac = Convert.ToDecimal(row["VL_Vac06"].ToString(), new CultureInfo("en-US")),
+                        VL_Fac = Convert.ToDecimal(row["VL_Fac06"].ToString(), new CultureInfo("en-US")),
+                        VL_Pac = Convert.ToDecimal(row["VL_Pac06"].ToString(), new CultureInfo("en-US")),
+                        VL_Vdc = Convert.ToDecimal(row["VL_Vdc06_01"].ToString(), new CultureInfo("en-US")),
+                        VL_Iac = Convert.ToDecimal(row["VL_Iac06"].ToString(), new CultureInfo("en-US")),
+                        Total = Convert.ToDecimal(row["VL_ETotal06"].ToString(), new CultureInfo("en-US")),
                     };
                     meteo.Onduleurs.Add(onduleur6);
 
